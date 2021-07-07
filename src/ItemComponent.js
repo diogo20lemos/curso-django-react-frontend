b3845417917e87d413c0ed6f77876d1bb4954469
Cicero Diogo Lemos <diogolemos@EDiagnose.local>
2021-07-07 12:47:18 -0300

    Fixing an issue on Items

--- src/ItemComponent.js
+++ src/ItemComponent.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function ItemComponent(props){
     const status = props.status;
     return <li>{props.name}
-            <p>Status: {status ? <div>Finalizado</div> : <div>Não Finalizado</div> }</p>
+            <div>Status: {status ? <div>Finalizado</div> : <div>Não Finalizado</div> }</div>
            </li>
 }
